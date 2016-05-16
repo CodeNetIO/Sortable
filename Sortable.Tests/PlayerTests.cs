4136bbc5ae7e46c8b9bf7c8f4259182fa1f15e4e
Darin Hoover <darin@codenet.io>
2016-05-15 19:50:34 -0700

    Initial commit.

---
+++ Sortable.Tests/PlayerTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sortable.Tests
+{
+    [TestClass]
+    public class PlayerTests
+    {
+        #region Utilities
+
+        private List<Player> CreatePlayersWithInitiatives(int numberOfPlayers)
+        {
+            List<Player> result = new List<Player>(numberOfPlayers);
+
+            for (var i = 0; i < numberOfPlayers; i++)
+            {
+                var initiative = new Random().Next(1,20);
+                // unique initiative
+                while (result.Any(p => p.Initiative == initiative))
+                {
+                    initiative = new Random().Next(1, 20);
+                }
+                result.Add(new Player(initiative));
+            }
+
+            return result;
+        }
+        #endregion
+
+        [TestMethod]
+        public void InitiativeOrderTest()
+        {
+            var players = CreatePlayersWithInitiatives(8);
+
+            // sort ascending
+            // players.Sort();
+
+            // sort descending
+            players.Sort((a, b) => -1*a.CompareTo(b));
+
+            for (var i = 0; i < players.Count - 1; i++)
+            {
+                if (players[i].Initiative <= players[i + 1].Initiative)
+                {
+                    Assert.Fail("Order is not descending.");
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ Sortable/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sortable
+{
+    public class Player : IComparable<Player>
+    {
+        public Player(int initiative)
+        {
+            Initiative = initiative;
+        }
+
+        /// <summary>
+        /// Gets or sets a value representing a player's encounter initiative.
+        /// </summary>
+        /// <remarks>A higher initiative means the player goes first.</remarks>
+        public int Initiative { get; }
+
+        /// <summary>Compares the current player's initiative with another players.</summary>
+        /// <returns>0 if Initiatives are equal, +1 if this Initiative is greater than the other's, and -1 if it is less.</returns>
+        /// <param name="other">A player to compare initiatives with.</param>
+        public int CompareTo(Player other)
+        {
+            if (this.Initiative == other.Initiative)
+                return 0;
+            else if (this.Initiative > other.Initiative)
+                return 1;
+            else if (this.Initiative < other.Initiative)
+                return -1;
+            else 
+                return -1;
+        }
+    }
+}
