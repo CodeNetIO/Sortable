0fcd119804829c7c9fb2c27d209ba9846c063881
Darin Hoover <darin@codenet.io>
2016-05-16 20:38:45 -0700

    Shortened line-lengths.

--- Sortable/CompareInitiativeDescending.cs
+++ Sortable/CompareInitiativeDescending.cs
@@ -1,39 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sortable
 {
     public class CompareInitiativeDescending : IComparer<Player>
     {
-        /// <summary>Compares the iniatives of two players and returns results descendingly.</summary>
-        /// <returns>A signed integer that indicates the relative values of <paramref name="x" />
-        /// and <paramref name="y" />, as shown in the following table.  Value Meaning Less than 
-        /// zero<paramref name="x" /> is greater than <paramref name="y" />.  Zero<paramref name="x" /> 
-        /// equals <paramref name="y" />.  Greater than zero<paramref name="x" /> is less than 
-        /// <paramref name="y" />.</returns>
+        /// <summary>
+        /// Compares the iniatives of two players and returns results descendingly.
+        /// </summary>
         /// <param name="x">The first object to compare.</param>
         /// <param name="y">The second object to compare.</param> 
-        /// <exception cref="ArgumentException">x is not a <see cref="Player"/> OR y is not a <see cref="Player"/>.</exception>
+        /// <exception cref="ArgumentException">
+        /// x is not a <see cref="Player"/>
+        /// OR
+        /// y is not a <see cref="Player"/>.
+        /// </exception>
         public int Compare(object x, object y)
         {
             var playerX = (x as Player);
             var playerY = (y as Player);
             if (playerX == null || playerY == null)
             {
-                throw new ArgumentException("Both objects must be of type Player to be compared.");
+                throw new ArgumentException(
+                    "Both objects must be of type Player to be compared.");
             }
             return Compare(playerX, playerY);
         }
 
-        /// <summary>Compares the iniatives of two players and returns results descendingly.</summary>
-        /// <returns>A signed integer that indicates the relative values of <paramref name="x" />
-        /// and <paramref name="y" />, as shown in the following table.  Value Meaning Less than 
-        /// zero<paramref name="x" /> is greater than <paramref name="y" />.  Zero<paramref name="x" /> 
-        /// equals <paramref name="y" />.  Greater than zero<paramref name="x" /> is less than 
-        /// <paramref name="y" />.</returns>
+        /// <summary>
+        /// Compares the iniatives of two players and returns results descendingly.
+        /// </summary>
         /// <param name="x">The first object to compare.</param>
         /// <param name="y">The second object to compare.</param>
         public int Compare(Player x, Player y)
--- Sortable/Player.cs
+++ Sortable/Player.cs
@@ -34,7 +34,6 @@ namespace Sortable
         {
             ExperiencePoints += exp;
         }
-        #endregion
 
         /// <summary>
         /// Randomly sets the initiative to a number between 1 and 20 inclusively.
@@ -43,24 +42,36 @@ namespace Sortable
         {
             Initiative = new Random().Next(1, 20);
         }
+        #endregion
 
         #region IComparable Implementation
-        /// <summary>Compares the current player's experience with another player's.</summary>
-        /// <returns>0 if experience is equal, +1 if greater than the other, or -1 if less.</returns>
+        /// <summary>
+        /// Compares the current player's experience with another player's.
+        /// </summary>
+        /// <returns>
+        /// 0 if experience is equal, +1 if greater than the other, or -1 if less.
+        /// </returns>
         /// <param name="other">A player to compare experience with.</param>
-        /// <exception cref="ArgumentException">Object is not a <see cref="Player"/>.</exception>
+        /// <exception cref="ArgumentException">
+        /// Object is not a <see cref="Player"/>.
+        /// </exception>
         public int CompareTo(object other)
         {
             var player = (other as Player);
             if (player == null)
             {
-                throw new ArgumentException("Object to be compared is not a player.", nameof(player));
+                throw new ArgumentException(
+                    "Object to be compared is not a player.", nameof(player));
             }
             return CompareTo(player);
         }
 
-        /// <summary>Compares the current player's experience with another player's.</summary>
-        /// <returns>0 if experience is equal, +1 if greater than the other, or -1 if less.</returns>
+        /// <summary>
+        /// Compares the current player's experience with another player's.
+        /// </summary>
+        /// <returns>
+        /// 0 if experience is equal, +1 if greater than the other, or -1 if less.
+        /// </returns>
         /// <param name="other">A player to compare experience with.</param>
         public int CompareTo(Player other)
         {
